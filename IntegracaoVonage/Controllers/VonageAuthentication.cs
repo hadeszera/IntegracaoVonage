acb55f35b08f090797cecdbbb3493ba48d94dc34
Daniel Batista <danielcencibatista@gmail.com>
2021-06-29 09:14:33 -0300

    Adicionar arquivos de projeto.

---
+++ IntegracaoVonage/Controllers/VonageAuthentication.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenTokSDK;
+using Service;
+using System.Dynamic;
+
+namespace IntegracaoVonage.Controllers
+{
+    [Route("v1/Vonage")]
+    [ApiController]
+    public class VonageAuthentication : ControllerBase
+    {
+        private readonly OpenTokService openTokService;
+
+        public VonageAuthentication(OpenTokService openTokService)
+        {
+            this.openTokService = openTokService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> InitSession()
+        {
+            dynamic locals = new ExpandoObject();
+            locals.ApiKey = openTokService.OpenTok.ApiKey.ToString();
+            locals.SessionId = openTokService.Session.Id;
+            locals.Token = openTokService.Session.GenerateToken();
+            return Ok(locals);
+        }
+
+
+
+
+
+
+
+
+      
+    }
+}
---
+++ Service/OpenTokService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenTokSDK;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class OpenTokService
+    {
+
+
+        private readonly IConfiguration _configuration;
+
+        public OpenTokService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Session Session { get; protected set; }
+        public OpenTok OpenTok { get; protected set; }
+
+        
+
+        public void InitSessionOpenTok()
+        {
+            int apiKey = 0;
+            string apiSecret = null;
+            try
+            {
+                string apiKeyString = _configuration.GetSection("OpenTok")["API_KEY"];
+                apiSecret = _configuration.GetSection("OpenTok")["API_SECRET"];
+                apiKey = Convert.ToInt32(apiKeyString);
+            }
+
+            catch (Exception ex)
+            {
+                if (!(ex is ConfigurationErrorsException || ex is FormatException || ex is OverflowException || ex is NullReferenceException))
+                {
+                    throw ex;
+                }
+            }
+
+            finally
+            {
+                if (apiKey == 0 || apiSecret == null)
+                {
+                    Console.WriteLine(
+                        "The OpenTok API Key and API Secret were not set in the application configuration. " +
+                        "Set the values in App.config and try again. (apiKey = {0}, apiSecret = {1})", apiKey, apiSecret);
+                    Console.ReadLine();
+                    Environment.Exit(-1);
+                }
+            }
+
+            this.OpenTok = new OpenTok(apiKey, apiSecret);
+
+            this.Session = this.OpenTok.CreateSession();
+        }
+    }
+}
